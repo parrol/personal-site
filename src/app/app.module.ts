3c2a135784962eed3ffbe8f0cb5df32fc1224e74
Carlos Parra <carlos.parra97@gmail.com>
2021-12-12 20:44:51 -0400

    refactor:  restructure home page and add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SiteUnderConstructionComponent } from './components/site-under-construction/site-under-construction.component';
+import { ProjectComponent } from './components/project/project.component';
+import { ProjectsSectionComponent } from './components/projects-section/projects-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SiteUnderConstructionComponent,
+    ProjectComponent,
+    ProjectsSectionComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
